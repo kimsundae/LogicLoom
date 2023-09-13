236b6068e07d5619344bdfd4c8ceaaf691066427
kimgeunbae <naa159@naver.com>
2023-09-13 15:25:41 +0900

    https://www.acmicpc.net/problem/14425 푸는 중

--- dataStructures_algorithms/src/backjoon/nowSolving/Main.java
+++ dataStructures_algorithms/src/backjoon/nowSolving/Main.java
@@ -1,10 +1,43 @@
 package backjoon.nowSolving;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main(String[] args) throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer( bf.readLine() , " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		String[] set = new String[N+M];
+		
+		int index = N;
+		
+		
+		for(int i=0; i<N; i++) 
+			set[i] = bf.readLine();
+		
+		for(int i=N; i<=M+N; i++) {
+						
+			boolean result = false;
+			String temp = bf.readLine();
+			for(int j=0; j<N; j++) {
+				if(set[j].equals(temp))
+					result = true;
+					
+			}
+			if( !result ) {
+				set[index++] = temp;
+			}
+				
+			
+				
+		}
+			
+	}		
 }
