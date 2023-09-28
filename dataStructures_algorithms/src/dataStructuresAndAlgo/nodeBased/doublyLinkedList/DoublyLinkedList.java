42b1430724d7d6d3f6395a5d050c7f4564726d47
kimgeunbae <naa159@naver.com>
2023-09-28 19:48:18 +0900

    19:48 이중 연결리스트 노드와 매핑 클래스 작성

--- dataStructures_algorithms/src/dataStructuresAndAlgo/nodeBased/doublyLinkedList/DoublyLinkedList.java
+++ dataStructures_algorithms/src/dataStructuresAndAlgo/nodeBased/doublyLinkedList/DoublyLinkedList.java
@@ -1,5 +1,31 @@
 package dataStructuresAndAlgo.nodeBased.doublyLinkedList;
 
 public class DoublyLinkedList {
+	private DoublyLinkedList_Node firstNode;
+	private DoublyLinkedList_Node lastNode;
+	
+	public DoublyLinkedList() { firstNode = null; lastNode = null;}
 
+	public DoublyLinkedList_Node getFirstNode() {
+		return firstNode;
+	}
+	
+	public void setFirstNode(DoublyLinkedList_Node firstNode) {
+		this.firstNode = firstNode;
+	}
+
+	public DoublyLinkedList_Node getLastNode() {
+		return lastNode;
+	}
+
+	public void setLastNode(DoublyLinkedList_Node lastNode) {
+		this.lastNode = lastNode;
+	}
+
+	@Override
+	public String toString() {
+		return "DoublyLinkedList [firstNode=" + firstNode + ", lastNode=" + lastNode + "]";
+	}
+	
+	
 }
--- dataStructures_algorithms/src/dataStructuresAndAlgo/nodeBased/doublyLinkedList/DoublyLinkedList_Node.java
+++ dataStructures_algorithms/src/dataStructuresAndAlgo/nodeBased/doublyLinkedList/DoublyLinkedList_Node.java
@@ -1,5 +1,39 @@
 package dataStructuresAndAlgo.nodeBased.doublyLinkedList;
 
 public class DoublyLinkedList_Node {
-
+	private String data;
+	private DoublyLinkedList_Node nextNode;
+	private DoublyLinkedList_Node previousNode;
+	public DoublyLinkedList_Node() {}
+	public DoublyLinkedList_Node(String data, DoublyLinkedList_Node nextNode, DoublyLinkedList_Node previousNode) {
+		super();
+		this.data = data;
+		this.nextNode = nextNode;
+		this.previousNode = previousNode;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public DoublyLinkedList_Node getnextNode() {
+		return nextNode;
+	}
+	public void setnextNode(DoublyLinkedList_Node nextNode) {
+		this.nextNode = nextNode;
+	}
+	public DoublyLinkedList_Node getpreviousNode() {
+		return previousNode;
+	}
+	public void setpreviousNode(DoublyLinkedList_Node previousNode) {
+		this.previousNode = previousNode;
+	}
+	@Override
+	public String toString() {
+		return "DoublyLinkedList_Node [data=" + data + ", nextNode=" + nextNode + ", previousNode=" + previousNode
+				+ "]";
+	}
+	
+		
 }
