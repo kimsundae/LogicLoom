1f6831f03c8373753fad6d558141d25ee4b46e96
kimgeunbae <naa159@naver.com>
2023-10-21 22:21:36 +0900

    이진 힙 연습문제 2번

---
+++ dataStructures_algorithms/src/dataStructuresAndAlgo/nodeBased/binaryHeap/PracticeProblem_2.java
@@ -0,0 +1,13 @@
+package dataStructuresAndAlgo.nodeBased.binaryHeap;
+/*
+    373p 연습문제 2번
+            1번 문제의 힙에서 루트 노드를 삭제하면 힙이 어떤 모습일까
+ */
+public class PracticeProblem_2 {
+    /*
+    *                       9
+    *               6               8
+    *           3       5       7       4
+    *       2      1
+    * */
+}
