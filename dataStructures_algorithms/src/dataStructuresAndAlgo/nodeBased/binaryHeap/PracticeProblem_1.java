6bda5e67e8477abe5c0c9a6827cdeafa6a7c409e
kimgeunbae <naa159@naver.com>
2023-10-21 22:19:18 +0900

    이진 힙 연습문제 1번

---
+++ dataStructures_algorithms/src/dataStructuresAndAlgo/nodeBased/binaryHeap/PracticeProblem_1.java
@@ -0,0 +1,18 @@
+package dataStructuresAndAlgo.nodeBased.binaryHeap;
+/*
+* 373p 연습문제 1.
+*       힙에 값 11을 삽입하면 힙이 어떤 모습일까.
+* */
+public class PracticeProblem_1 {
+    /*
+
+                         11
+                 10             8
+               6    9          7 4
+              2  1  3 5
+
+     */
+
+
+
+}
