389dc77d34d22317f773b0f643e09b1eb338e47a
kimgeunbae <naa159@naver.com>
2023-12-04 20:00:18 +0900

    17:23 백준 10816 풀이

--- dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/setAndMap/SetAndMap_10816.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/setAndMap/SetAndMap_10816.java
@@ -1,21 +1,26 @@
-package coddingProblem.backjoon.nowSolving;
+package coddingProblem.backjoon.categoryProblem.setAndMap;
 
 
-import java.io.*;
-import java.util.*;
-
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/*
+* 	https://www.acmicpc.net/problem/10816 숫자 카드 2
+* */
+public class SetAndMap_10816 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-
+		// 수의 범위 -10000000 ~ 10000000까지
 		int[] cards = new int[20000001];
 
 		int N = Integer.parseInt(br.readLine());
 
 		st = new StringTokenizer(br.readLine(), " ");
 
+		// 음수가 존재하므로 0부터 200000001까지
 		while( st.hasMoreTokens() ){
 			cards[ Integer.parseInt( st.nextToken() ) + 10000000  ]++;
 		}
