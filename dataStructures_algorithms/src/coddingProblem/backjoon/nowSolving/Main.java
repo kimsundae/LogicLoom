42f23244e475010c303b4ab359459a810d956919
kimgeunbae <naa159@naver.com>
2023-12-05 00:09:21 +0900

    12:09 백준 1764 풀이

--- dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
@@ -5,29 +5,69 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	/*public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int N = Integer.parseInt(st.nextToken()) , M = Integer.parseInt(st.nextToken());
+		// 명단
+		Set< String > names = new HashSet<>();
+		// 듣보잡 명단
+		TreeSet< String > noHearAndSeeNames = new TreeSet<>();
+		// 듣도 못한 사람 names 저장
+		for( int i = 0; i < N; i++ )
+			names.add(br.readLine());
+
+		// 보도 못한 사람 저장
+		for( int i = 0; i < M; i++ ){
+
+			String noSeePerson = br.readLine();
+			// 만약 보도못한 사람이 중복 된다면 듣보잡 명단에 추가
+			if( !names.add(noSeePerson) )
+				noHearAndSeeNames.add(noSeePerson);
+
+		}
+
+		Iterator< String > iterator = noHearAndSeeNames.iterator();
+		sb.append(noHearAndSeeNames.size()).append("\n");
+
+		while(iterator.hasNext() ){
+			sb.append(iterator.next()).append("\n");
+		}
+
+		System.out.println(sb);
+
+	}*/
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		int[] cards = new int[20000001];
+		int N = Integer.parseInt(st.nextToken()) , M = Integer.parseInt(st.nextToken());
+		// 명단
+		Set< String > names = new HashSet<>();
+		// 듣보잡 명단
+		List< String > noHearAndSeeNames = new ArrayList<>();
+		// 듣도 못한 사람 names 저장
+		for( int i = 0; i < N; i++ )
+			names.add(br.readLine());
 
-		int N = Integer.parseInt(br.readLine());
+		// 보도 못한 사람 저장
+		for( int i = 0; i < M; i++ ){
 
-		st = new StringTokenizer(br.readLine(), " ");
+			String noSeePerson = br.readLine();
+			// 만약 보도못한 사람이 중복 된다면 듣보잡 명단에 추가
+			if( !names.add(noSeePerson) )
+				noHearAndSeeNames.add(noSeePerson);
 
-		while( st.hasMoreTokens() ){
-			cards[ Integer.parseInt( st.nextToken() ) + 10000000  ]++;
 		}
 
-		int M = Integer.parseInt(br.readLine());
+		Collections.sort(noHearAndSeeNames);
 
-		st = new StringTokenizer(br.readLine(), " ");
-
-		while( st.hasMoreTokens() ){
-			int number = Integer.parseInt( st.nextToken() );
-			sb.append(cards[  number + 10000000 ]).append(" ");
-		}
+		sb.append(noHearAndSeeNames.size()).append("\n");
+		for(int i = 0; i < noHearAndSeeNames.size(); i++)
+			sb.append(noHearAndSeeNames.get(i)).append('\n');
 
 		System.out.println(sb);
 
---
+++ dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/SetAndMap_1764.java
@@ -0,0 +1,46 @@
+package coddingProblem.backjoon.nowSolving;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+/*
+* https://www.acmicpc.net/problem/1764 듣보잡
+* */
+public class SetAndMap_1764 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int N = Integer.parseInt(st.nextToken()) , M = Integer.parseInt(st.nextToken());
+		// 명단
+		Set< String > names = new HashSet<>();
+		// 듣보잡 명단
+		TreeSet< String > noHearAndSeeNames = new TreeSet<>();
+		// 듣도 못한 사람 names 저장
+		for( int i = 0; i < N; i++ )
+			names.add(br.readLine());
+
+		// 보도 못한 사람 저장
+		for( int i = 0; i < M; i++ ){
+
+			String noSeePerson = br.readLine();
+			// 만약 보도못한 사람이 중복 된다면 듣보잡 명단에 추가
+			if( !names.add(noSeePerson) )
+				noHearAndSeeNames.add(noSeePerson);
+
+		}
+
+		Iterator< String > iterator = noHearAndSeeNames.iterator();
+		sb.append(noHearAndSeeNames.size()).append("\n");
+
+		while(iterator.hasNext() ){
+			sb.append(iterator.next()).append("\n");
+		}
+
+		System.out.println(sb);
+
+	}
+}
