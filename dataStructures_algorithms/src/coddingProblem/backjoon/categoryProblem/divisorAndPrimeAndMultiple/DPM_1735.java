2b344eaf5e6309fa07fe3dd1f57952fca48a833f
kimgeunbae <naa159@naver.com>
2023-12-09 21:22:27 +0900

    21:22 백준 1735 풀이

--- dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/divisorAndPrimeAndMultiple/DPM_1735.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/divisorAndPrimeAndMultiple/DPM_1735.java
@@ -1,10 +1,14 @@
-package coddingProblem.backjoon.nowSolving;
+package coddingProblem.backjoon.categoryProblem.divisorAndPrimeAndMultiple;
 
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
-public class Main{
+/*
+*  https://www.acmicpc.net/problem/1735 분수 합
+* */
+public class DPM_1735 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
