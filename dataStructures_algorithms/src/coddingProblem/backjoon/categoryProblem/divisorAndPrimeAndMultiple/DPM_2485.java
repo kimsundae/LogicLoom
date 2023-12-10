be95379533f5c67cd7aeba2d3aa80c7c4bb1af9e
kimgeunbae <naa159@naver.com>
2023-12-10 17:54:36 +0900

    17:54 백준 2485 풀이

--- dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/divisorAndPrimeAndMultiple/DPM_2485.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/divisorAndPrimeAndMultiple/DPM_2485.java
@@ -1,10 +1,14 @@
-package coddingProblem.backjoon.nowSolving;
+package coddingProblem.backjoon.categoryProblem.divisorAndPrimeAndMultiple;
 
 
-import java.io.*;
-import java.util.*;
-
-public class Main{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/*
+* https://www.acmicpc.net/problem/2485 가로수
+* */
+public class DPM_2485 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
