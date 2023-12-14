87b8116db616ed61992f1bf649bcac3b4b380a5a
kimgeunbae <naa159@naver.com>
2023-12-14 21:54:11 +0900

    21:53 베르트랑 공준 풀이 중

--- dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
@@ -5,11 +5,25 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main{
-
+    public static boolean isPrime(int N){
+        if( 2 > N ) return false;
+        for( int i = 2; i*i < N; i++ ){
+            if( N % i == 0) return false;
+        }
+        return true;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int doubleN = N*2;
+        while( doubleN >= N ){
+            if(isPrime(N)){
+                sb.append(N).append("\n");
+            }
+            N++;
+        }
         //
     }
 }
