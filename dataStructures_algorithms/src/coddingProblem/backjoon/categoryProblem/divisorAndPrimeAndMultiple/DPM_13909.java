7c46f77bf63126f64ad4399dd7a199622348ee2c
kimgeunbae <naa159@naver.com>
2023-12-18 00:16:58 +0900

    00:16 백준 13909 풀이

--- dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/divisorAndPrimeAndMultiple/DPM_13909.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/divisorAndPrimeAndMultiple/DPM_13909.java
@@ -1,9 +1,12 @@
-package coddingProblem.backjoon.nowSolving;
+package coddingProblem.backjoon.categoryProblem.divisorAndPrimeAndMultiple;
 
 
-import java.io.*;
-
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+* https://www.acmicpc.net/problem/13909 창문 닫기
+* */
+public class DPM_13909 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
