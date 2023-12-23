415682a36de02912bc0a8bb25f2323bc2853071d
kimgeunbae <naa159@naver.com>
2023-12-24 01:27:35 +0900

    01:27 백준 11866 요세푸스 문제 0 풀이 중

--- dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
@@ -4,61 +4,10 @@ package coddingProblem.backjoon.nowSolving;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        customizeLinkedList customList = new customizeLinkedList();
-        for( int i = 1; i <= N; i++ ){
-            customList.addNode(i);
-        }
-        while( customList.firstNode != customList.LastNode ){
-            customList.delete();
-            customList.addNode(customList.pop().value);
-        }
-        System.out.println(customList.firstNode.value);
-    }
 
-    static class customizeLinkedList{
-        Node firstNode;
-        Node LastNode;
-        public customizeLinkedList(){
-            firstNode = null;
-            LastNode = null;
-        }
-        public void read(){
-            Node currentNode = firstNode;
-            while(currentNode != null){
-                System.out.println(currentNode.value);
-                currentNode = currentNode.nextNode;
-            }
-        }
-        public void addNode( int num ){
-            if( firstNode == null ) {
-                Node node = new Node(num, null);
-                firstNode = node;
-                LastNode = node;
-            }else{
-                Node node = new Node( num, null );
-                LastNode.nextNode = node;
-                LastNode = node;
-            }
-        }
-        public Node pop(){
-            Node node = firstNode;
-            firstNode = node.nextNode;
-            return node;
-        }
-        public void delete(){
-            firstNode = firstNode.nextNode;
-        }
-    }
-    static class Node{
-        int value;
-        Node nextNode;
-        public Node( int num, Node node ){
-            this.value = num;
-            this.nextNode = node;
-        }
     }
 }
