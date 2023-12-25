cc148ec6681561a6b979cec6b63128ba04016ff3
kimgeunbae <naa159@naver.com>
2023-12-25 23:36:39 +0900

    23:36 백준 11866 요세푸스 문제0 풀이

--- dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/stack_queue_deque/Queue_11866.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/stack_queue_deque/Queue_11866.java
@@ -1,10 +1,13 @@
-package coddingProblem.backjoon.nowSolving;
+package coddingProblem.backjoon.categoryProblem.stack_queue_deque;
 
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
-public class Main {
+/*
+*  https://www.acmicpc.net/problem/11866 요세푸스 문제0
+* */
+public class Queue_11866 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
