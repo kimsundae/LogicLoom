a705e7678908224246f9c611d202408ca0d09c9f
kimgeunbae <naa159@naver.com>
2023-12-29 18:19:27 +0900

    18:19 백준 24723 녹색거탑 풀이

--- dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/combinatorics/combinatorics_24723.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/combinatorics/combinatorics_24723.java
@@ -1,10 +1,12 @@
-package coddingProblem.backjoon.nowSolving;
+package coddingProblem.backjoon.categoryProblem.combinatorics;
 
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
-public class Main{
+/*
+*  https://www.acmicpc.net/problem/24723 녹색거탑
+* */
+public class combinatorics_24723 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int top = Integer.parseInt(br.readLine());
