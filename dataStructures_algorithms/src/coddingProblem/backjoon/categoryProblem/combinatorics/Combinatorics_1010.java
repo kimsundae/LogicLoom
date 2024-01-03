8e83e78a830ad8ab48ae6da031684e93ac8a8419
kimgeunbae <naa159@naver.com>
2024-01-03 23:34:35 +0900

    23:34 백준 1010 다리 놓기 풀이

--- dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/combinatorics/Combinatorics_1010.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/combinatorics/Combinatorics_1010.java
@@ -1,12 +1,14 @@
-package coddingProblem.backjoon.nowSolving;
+package coddingProblem.backjoon.categoryProblem.combinatorics;
 
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
-
-public class Main{
+/*
+*  https://www.acmicpc.net/problem/1010 다리 놓기
+* */
+public class Combinatorics_1010 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
