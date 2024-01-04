71b54c3b20220f71705e860663b08b36b9572112
kimgeunbae <naa159@naver.com>
2024-01-04 23:26:48 +0900

    23:26 백준 1037 약수 풀이

--- dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/combinatorics/Combinatiorics_1037.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/combinatorics/Combinatiorics_1037.java
@@ -1,12 +1,14 @@
-package coddingProblem.backjoon.nowSolving;
+package coddingProblem.backjoon.categoryProblem.combinatorics;
 
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
-public class Main{
+/*
+* https://www.acmicpc.net/problem/1037 약수
+* */
+public class Combinatiorics_1037 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
