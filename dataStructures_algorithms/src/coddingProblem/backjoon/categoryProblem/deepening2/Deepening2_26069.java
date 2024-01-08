63bcc6a7245d3be74bd2699593cecf4d8282e0f6
kimgeunbae <naa159@naver.com>
2024-01-09 00:26:07 +0900

    00:25 백준 26069 붙임성 좋은 총총이

--- dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/deepening2/Deepening2_26069.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/deepening2/Deepening2_26069.java
@@ -1,12 +1,14 @@
-package coddingProblem.backjoon.nowSolving;
+package coddingProblem.backjoon.categoryProblem.deepening2;
 
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
-
-public class Main{
+/*
+* https://www.acmicpc.net/problem/26069 붙임성 좋은 총총이
+* */
+public class Deepening2_26069 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] names;
@@ -14,7 +16,7 @@ public class Main{
         nameSet.add("ChongChong");
         int N = Integer.parseInt(br.readLine());
         int count = 1;
-
+        
         while( N-- > 0 ){
 
             names = br.readLine().split(" ");
