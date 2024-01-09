05eeb2474c0a4e445a5c880c6c83659cc344a739
kimgeunbae <naa159@naver.com>
2024-01-09 21:27:15 +0900

    21:27 백준 2108 풀이 중

--- dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
@@ -8,13 +8,38 @@ public class Main{
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        if( N == 0 || N == 1 )
-            System.out.println(1);
-        else{
-            int sum = 1;
-            for( int i = 0; i < N; i++ )
-                sum *= N-i;
-            System.out.println(sum);
+        int[] arr = new int[8001];
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int manyAppears = 0, manyAppearsIndex = 0;
+
+        for( int i = 0; i < N; i++){
+            int inputNum = Integer.parseInt(br.readLine());
+            sum += inputNum;
+
+            if( min > inputNum )
+                min = inputNum;
+            if( max < inputNum )
+                max = inputNum;
+
+            arr[inputNum+4000]++;
         }
+
+        int[] newArr = new int[N];
+        int index = 0;
+        for( int i = 0; i < arr.length; i++ ){
+            int temp = arr[i];
+            if(temp != 0) {
+
+                if( temp > manyAppears ) {
+                    manyAppears = temp;
+                    manyAppearsIndex = i-4000;
+                }
+                newArr[index++] = temp;
+            }
+        }
+
+        System.out.println( Math.round(sum / (double)N) +"\n"+newArr[N/2]+"\n"+manyAppearsIndex+"\n"+(max-min));
     }
 }
