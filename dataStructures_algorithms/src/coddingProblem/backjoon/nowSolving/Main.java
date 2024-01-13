0701682074a26adf45f1f9275868d69743813833
kimgeunbae <naa159@naver.com>
2024-01-13 16:32:34 +0900

    16:32 병합 정렬 학습 중

--- dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
@@ -2,27 +2,72 @@ package coddingProblem.backjoon.nowSolving;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main{
-
-    public static String recursion( String S, int l, int r, int callCount ){
-        callCount++;
-        if(l >= r) return 1+" "+callCount;
-        else if(S.charAt(l) != S.charAt(r)) return 0+" "+callCount;
-        else return recursion(S, l+1, r-1, callCount);
+    static int[] sortedArr = null;
+    static int count = 0;
+    static int K = 0;
+    public static void sort(int[] arr, int left, int right) {
+        sortedArr = new int[arr.length];
+        mergeSort(arr, left, right);
     }
-    public static String isPalindrome( String S ){
-        return recursion(S, 0, S.length()-1, 0);
+    public static void mergeSort(int[] arr, int left, int right){
+
+        if(left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+    public static void merge(int[] arr, int left, int mid, int right){
+
+        int l = left;
+        int r = mid+1;
+        int sortedIndex = left;
+        while(l <= mid && r <= right){
+            if(arr[l] <= arr[r])
+                sortedArr[sortedIndex++] = arr[l++];
+            else
+                sortedArr[sortedIndex++] = arr[r++];
+        }
+        if(l > mid){
+            for(int i = r; i <= right; i++){
+                sortedArr[sortedIndex] = arr[i];
+            }
+        }else {
+            for(int i = l; i <= mid; i++){
+                sortedArr[sortedIndex++] = arr[i];
+            }
+        }
+
+        for(int i=left; i<=right; i++) {
+            if(++count == K)
+                System.out.println(sortedArr[i]);
+            arr[i] = sortedArr[i];
+        }
+
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        while( N-- > 0 ){
-            String S = br.readLine();
-            sb.append(isPalindrome(S)).append("\n");
-        }
-        System.out.println(sb);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[N]; int index = 0;
+        for(int i = 0; i < arr.length; i++)
+            arr[index++] = Integer.parseInt(st.nextToken());
+
+        sort(arr, 0 , arr.length-1);
+        if( K > count )
+            System.out.println(-1);
+        System.out.println(Arrays.toString(sortedArr));
+
     }
 }
