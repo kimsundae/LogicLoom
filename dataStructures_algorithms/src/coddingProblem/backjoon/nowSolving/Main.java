400d7daa159f75fc741ea50e2b75c26b985e9569
kimgeunbae <naa159@naver.com>
2024-01-14 17:54:55 +0900

    17:54 백준 4779 칸토어집합 풀이 중

--- dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
@@ -3,88 +3,65 @@ package coddingProblem.backjoon.nowSolving;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
+    static char[] kantoa;
+    // 초기 0, kantoa.length - 1
+    static void kantoa(int left, int right){
+        System.out.println("left = " + left);
+        System.out.println("right = " + right);
+        for(int i = 0; i<kantoa.length; i++)
+            System.out.print(kantoa[i]);
+        System.out.println();
+        if(left < right) {
 
-    private static int[] sorted;
-    static int count = 0;
-    static int K;
+            int part = (right + 1 - left) / 3;
+            int temp = part+left;
+            int index = left + part;
+
+            while (part-- > 0) {
+                kantoa[index++] = ' ';
+            }
+
+            kantoa(left, temp - 1);
+            kantoa(index, right);
+
+        }
+
+        // 0, 8
+        // size == right + 1
+        // size / 3 == 3
+        // left + 3 == 3
+        // 3, 4, 5
+        // 6, 7, 8
+        // replace 3, 4, 5
+
+        // 0, 2
+        // size == right + 1
+        // size / 3 == 1
+        // 1, 1
+
+    }
 
     public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[N]; int index = 0;
-        for(int i = 0; i < arr.length; i++)
-            arr[index++] = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
 
-        merge_sort(arr);
-        if( K > count )
-            System.out.println(-1);
+        String input = "";
+        while( (input = br.readLine()) != null && !input.isEmpty() ){
 
-        System.out.println(Arrays.toString(arr));
-    }
-    public static void merge_sort(int[] a) {
+            int length = (int)Math.pow(3,Double.parseDouble(input));
 
-        sorted = new int[a.length];
-        merge_sort(a, 0, a.length - 1);
-    }
-    private static void merge_sort(int[] a, int left, int right) {
+            kantoa = new char[length];
 
-        for(int size = 1; size <= right; size += size) {
+            for (int i = 0; i < length; i++)
+                kantoa[i] = '-';
 
-            for(int l = 0; l <= right - size; l += (2 * size)) {
-                int low = l;
-                int mid = l + size - 1;
-                int high = Math.min(l + (2 * size) - 1, right);
-                merge(a, low, mid, high);		// 병합작업
-            }
+            kantoa( 0, kantoa.length-1 );
+            for(int i = 0; i<kantoa.length; i++)
+                sb.append(kantoa[i]);
+            sb.append("\n");
         }
-
-    }
-    private static void merge(int[] a, int left, int mid, int right) {
-        int l = left;
-        int r = mid + 1;
-        int idx = left;
-
-
-        while(l <= mid && r <= right) {
-            if(a[l] <= a[r]) {
-                sorted[idx] = a[l];
-                idx++;
-                l++;
-            }
-            else {
-                sorted[idx] = a[r];
-                idx++;
-                r++;
-            }
-        }
-
-
-        if(l > mid) {
-            while(r <= right) {
-                sorted[idx] = a[r];
-                idx++;
-                r++;
-            }
-        }
-
-        else {
-            while(l <= mid) {
-                sorted[idx] = a[l];
-                idx++;
-                l++;
-            }
-        }
-
-        for(int i = left; i <= right; i++) {
-            if( ++count == K )
-                System.out.println(sorted[i]);
-            a[i] = sorted[i];
-        }
+        System.out.println(sb);
     }
 }
