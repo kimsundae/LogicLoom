b1e4fc903b837311d62dcbf7fe813a9bca721e54
kimgeunbae <naa159@naver.com>
2024-01-18 17:26:57 +0900

    17:26 하노이의 탑 이동 순서

---
+++ dataStructures_algorithms/src/coddingProblem/backjoon/categoryProblem/recursion/Recursion_11729.java
@@ -0,0 +1,30 @@
+package coddingProblem.backjoon.categoryProblem.recursion;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+*  하노이 탑 이동 순서
+* */
+public class Recursion_11729 {
+
+    static StringBuilder sb = new StringBuilder();
+    static int count = 0;
+    public static void hanoi( int N, int from, int sub, int to ){
+        count++;
+        if( N == 1 ){
+            sb.append(from).append(" ").append(to).append("\n");
+            return;
+        }
+        hanoi( N-1, from, to, sub );
+        sb.append(from).append(" ").append(to).append("\n");
+        hanoi( N-1, sub, from, to);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        hanoi( N, 1, 2, 3 );
+        System.out.println(count + "\n" + sb.toString());
+    }
+}
--- dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
+++ dataStructures_algorithms/src/coddingProblem/backjoon/nowSolving/Main.java
@@ -3,115 +3,29 @@ package coddingProblem.backjoon.nowSolving;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-
 public class Main {
-
-    static int N;
-    static int count = 0;
-    static StringBuilder sb = new StringBuilder();
-
-    public static void hanoi( Stack A, Stack B, Stack C, int count, StringBuilder sb ){
-
-        if( C.isFull() ) {
-
-            if( Main.count > count  ) {
-                Main.count = count;
-                Main.sb = sb;
-            }
-            return;
-        }
-
-        if( !A.isEmpty() ){
-
-            Stack tempA = new Stack(A);
-            int temp =  tempA.pop();
-
-            if( B.isEmpty() || B.pick() > temp )
-                hanoi( tempA, new Stack(B, temp), C, count++, new StringBuilder(sb.append("1 2").append("\n")) );
-
-            if( C.isEmpty() || C.pick() > temp)
-                hanoi( tempA, B, new Stack(C, temp), count++, new StringBuilder(sb.append("1 3").append("\n")));
-
-        }
-        if( !B.isEmpty() ){
-
-            Stack tempB = new Stack(B);
-            int temp =  tempB.pop();
-
-            if( A.isEmpty() || A.pick() > temp )
-                hanoi( new Stack(A, temp), tempB, C, count++, new StringBuilder(sb.append("2 1").append("\n")) );
-
-            if( C.isEmpty() || C.pick() > temp)
-                hanoi( A, tempB, new Stack(C, temp), count++, new StringBuilder(sb.append("2 3").append("\n")));
-
-        }
-        if( !C.isEmpty() ){
-
-            Stack tempC = new Stack(C);
-            int temp =  tempC.pop();
-
-            if( A.isEmpty() || A.pick() > temp )
-                hanoi( A, new Stack(B, temp), tempC, count++, new StringBuilder(sb.append("3 2").append("\n")) );
-
-            if( C.isEmpty() || C.pick() > temp)
-                hanoi( new Stack(A, temp), B, tempC, count++, new StringBuilder(sb.append("3 1").append("\n")));
-
-        }
-
-    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = 5;
+        for( int i = 0; i < n; i++ ){
 
-        N = n;
-
-        Stack stack = new Stack(N);
-        while(n-->0){
-            stack.add(n+1);
+            for( int j = 0; j < i; j++ ){
+                System.out.print(" ");
+            }
+            for( int k = 0; k < 2 * (n - i) - 1; k++ ){
+                System.out.print("*");
+            }
+            System.out.println();
         }
-
-        hanoi( stack, new Stack(N), new Stack(N), 0, new StringBuilder() );
-        System.out.println(sb);
-/*        for(int i = 0; i < stack.stack.length; i++){
-            System.out.println(stack.stack[i]);
-        }*/
-
-    }
-
-    static class Stack{
-
-        int[] stack;
-        int index;
-
-        Stack(int capacity){
-            this.stack = new int[capacity];
-            this.index = -1;
-        }
-        Stack(Stack stack){
-            this.stack = stack.stack;
-            this.index = stack.index;
-        }
-        Stack(Stack stack, int num){
-            this.stack = stack.stack;
-            this.index = stack.index;
-            this.stack[++index] = num;
-        }
-        public void add(int num){
-            stack[++index] = num;
-        }
-        public int pop(){
-            return stack[index--];
-        }
-        public int pick(){
-            return stack[index];
-        }
-        public boolean isFull(){
-            return index == stack.length-1;
-        }
-        public boolean isEmpty(){
-            return index == -1;
+        for( int i = 1; i < n; i++ ){
+            for (int j = n-i-1; j > 0; j--){
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i +1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
-
 }
